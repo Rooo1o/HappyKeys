22788c7d9f6d82e6b7059dc50a8c6d9324292eb9
Rooo1o <32430285+Rooo1o@users.noreply.github.com>
2019-02-25 01:22:03 +0100

    Eerste liedje (ode to joy)
    Gewoon hardcoded in vexflowcontent.component.ts, was enkel om uit te proberen...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md'
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import * as _ from 'vexflow';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
+import { VexflowcontentComponent } from './vexflowcontent/vexflowcontent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    ContentComponent
+    ContentComponent,
+    VexflowcontentComponent
   ],
   imports: [
     BrowserModule, MDBBootstrapModule.forRoot()
